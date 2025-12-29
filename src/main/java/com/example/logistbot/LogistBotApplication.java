0721e7cb20fbf845b5dad26b10053e69a7e894ca
Shaxzod <ramazanovshaxzodbek@gmail.com>
2025-12-29 17:54:30 +0500

    Add project files

---
+++ src/main/java/com/example/logistbot/LogistBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.logistbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogistBotApplication.class, args);
+    }
+
+}
